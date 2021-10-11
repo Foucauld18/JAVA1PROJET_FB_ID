318485a02c6657d4261c6598657a7a2692ca37c3
Foucauld18 <71766032+Foucauld18@users.noreply.github.com>
2021-10-11 16:54:14 +0200

    PersonneJava

--- src/java1projet_fb_id/Personne.java
+++ src/java1projet_fb_id/Personne.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package java1projet_fb_id;
+import java.time.LocalDate;
+
 
 /**
  *
@@ -11,4 +13,21 @@ package java1projet_fb_id;
  */
 public class Personne {
     
+    protected String nomNaissance, lieuNaissance;
+    protected LocalDate dateNaissance, dateDeces;
+    String nomCourant, prenom, surnom, nationalite;
+    Float taille, poids;
+    
+    public Personne(String nomNaissance){
+    this.nomNaissance = nomNaissance;
+    }
+    
+    
+    public String getnomNaissance(){return this.nomNaissance;}
+    
+    public String getlieuNaissance(){return this.lieuNaissance;}
+    
+    public static void main(String[] args) {
+    // TODO code application logic here
+    }
 }
